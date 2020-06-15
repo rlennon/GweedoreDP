764c889a3f08f40e26bc3d3b01e67be57074a06c
L00156767 <L00156767@student.lyit.ie>
2020-06-14 17:51:31 -0700

    Added code to log user activities to database (user login and unit test activities)

--- src/test/java/test/java/com/lyit/JdbcTest.java
+++ src/test/java/test/java/com/lyit/JdbcTest.java
@@ -8,18 +8,22 @@ public class JdbcTest {
 
    @Test	
    public void testJdbc() {
-      try (
+      try {
+    	 Class.forName("com.mysql.cj.jdbc.Driver");
          Connection conn = DriverManager.getConnection(
                "jdbc:mysql://gweedore.cpagctkouszj.us-east-2.rds.amazonaws.com:3306/gweedore_db?useSSL=false",
                "admin", "sjn496gpDh0jM5N4r6Ib");
          Statement stmt = conn.createStatement();
-      ) {
-         String strSelect = "INSERT INTO gweedore_db.user_log (user_name, activity, activity_date) VALUES ( \"GweedoreUser\", \"Login\", CURRENT_TIMESTAMP)";
+       
+         String strSelect = "INSERT INTO gweedore_db.user_log (user_name, activity, activity_date) VALUES ( \"GweedoreDev\", \"UnitTest\", CURRENT_TIMESTAMP)";
  
          int rset = stmt.executeUpdate(strSelect);
  
       } catch(SQLException ex) {
          ex.printStackTrace();
-      }
+      } catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
    }
 }
